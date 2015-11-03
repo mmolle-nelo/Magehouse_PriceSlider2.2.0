2a21a821fe1c048e1eaa5ba86681000bb4c58c5f
Martin Molle <m.molle@netzloesungen.de>
2015-11-03 17:56:27 +0100

    [BUGFIX] fixed access to non existing or uninitialized variables

--- app/code/community/Magehouse/Slider/Model/Catalog/Layer.php
+++ app/code/community/Magehouse/Slider/Model/Catalog/Layer.php
@@ -7,55 +7,57 @@
  * @package     Mage_Catalog
  * @author      Mrugesh Mistry <support@magehouse.com>
  */
-class Magehouse_Slider_Model_Catalog_Layer extends Mage_Catalog_Model_Layer
-{
-   
-   
+class Magehouse_Slider_Model_Catalog_Layer extends Mage_Catalog_Model_Layer {
+
+
+	protected $currentRate;
+
 	/*
 	* Add Filter in product Collection for new price
 	*
 	* @return object
 	*/
-    public function getProductCollection()
-    {
-        if (isset($this->_productCollections[$this->getCurrentCategory()->getId()])) {
-            $collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
-        } else {
-            $collection = $this->getCurrentCategory()->getProductCollection();
-            $this->prepareProductCollection($collection);
-            $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
-        }
-		
-		$this->currentRate = $collection->getCurrencyRate();
-		$max=$this->getMaxPriceFilter();
-		$min=$this->getMinPriceFilter();
-		
-		if($min && $max){
-			$collection->getSelect()->where(' final_price >= "'.$min.'" AND final_price <= "'.$max.'" ');
+	public function getProductCollection() {
+		if (isset($this->_productCollections[$this->getCurrentCategory()->getId()])) {
+			$collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
+		} else {
+			$collection = $this->getCurrentCategory()->getProductCollection();
+			$this->prepareProductCollection($collection);
+			$this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
 		}
-		
-        return $collection;
-    }
-	
-	
+
+		$this->currentRate = $collection->getCurrencyRate();
+		$max = $this->getMaxPriceFilter();
+		$min = $this->getMinPriceFilter();
+
+		if ($min && $max) {
+			$collection->getSelect()->where(' final_price >= "' . $min . '" AND final_price <= "' . $max . '" ');
+		}
+
+		return $collection;
+	}
+
+
 	/*
 	* convert Price as per currency
 	*
 	* @return currency
 	*/
-	public function getMaxPriceFilter(){
-		return round($_GET['max']/$this->currentRate);
+	public function getMaxPriceFilter() {
+		$max = isset($_GET['max']) ? abs($_GET['max']) : 999999;
+		return round($max / $this->currentRate);
 	}
-	
-	
+
+
 	/*
 	* Convert Min Price to current currency
 	*
 	* @return currency
 	*/
-	public function getMinPriceFilter(){
-		return round($_GET['min']/$this->currentRate);
+	public function getMinPriceFilter() {
+		$min = isseT($_GET['min']) ? abs($_GET['min']) : 0;
+		return round($min / $this->currentRate);
 	}
-    
-	
+
+
 }
